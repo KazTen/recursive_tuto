beb65fb442d51870c729d84c07d6fd5b3e22980b
kazzy <vinetengoo3014@gmail.com>
2017-10-28 10:04:08 +0900

    軸名を文字で渡すバージョン

--- hanoi/hanoi01.py
+++ hanoi/hanoi01.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 # 移動関数
-def move(n, start, end):
+def move(n, start, via, end):
     if n > 1:
-        move(n-1, start, 6-start-end)
+        move(n-1, start, end, via)
 
     desc = "{0}番を{1}軸から{2}軸へ移動".format(n, start, end)
     print(desc)
 
     if n > 1:
-        move(n-1, 6-start-end, end)
+        move(n-1, via, start, end)
 
 # メイン
 i = input("円盤の枚数を入力しなさい：")
 su = int(i)
 
 # 呼び出し
-move(su, 1, 3)
+move(su, "A", "B", "C")
 
 
