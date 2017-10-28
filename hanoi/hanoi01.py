32dac4637796d6a5be3f1f5dcba9ae91a0cf06a8
kazzy <vinetengoo3014@gmail.com>
2017-10-28 10:17:44 +0900

    グローバル変数を用いた移動回数の表示を追加

--- hanoi/hanoi01.py
+++ hanoi/hanoi01.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 # 移動関数
 def move(n, start, via, end):
+    global cnt
     if n > 1:
         move(n-1, start, end, via)
 
-    desc = "{0}番を{1}軸から{2}軸へ移動".format(n, start, end)
+    desc = "{3}回目：{0}番を{1}から{2}へ移動".format(n, start, end, cnt)
     print(desc)
+    cnt=cnt+1
 
     if n > 1:
         move(n-1, via, start, end)
@@ -13,7 +15,7 @@ def move(n, start, via, end):
 # メイン
 i = input("円盤の枚数を入力しなさい：")
 su = int(i)
-
+cnt = 1
 # 呼び出し
 move(su, "木下", "中谷", "齋藤")
 
