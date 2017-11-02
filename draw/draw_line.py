a62d31095209d2f24df5c8710c6fa14fccec5c37
kazzy <vinetengoo3014@gmail.com>
2017-11-02 17:24:42 +0900

    曲げ角度を入力できるようにした 角度によって折り曲げる回数を変更した

--- draw/draw_line.py
+++ draw/draw_line.py
@@ -1,29 +1,48 @@
 # -*- coding: utf-8 -*-
 from turtle import *
 
-def recur (length, depth=0):
+def recur (length, depth=0, ang=60):
     if depth <= 0 :
         forward(length)
     else :
-        recur(length / 3, depth - 1)
-        left(60)
-        recur(length / 3, depth - 1)
-        left(-120)
-        recur(length / 3, depth -1)
-        left(60)
-        recur(length / 3, depth - 1)
+        recur(length / 3, depth - 1, ang)
+        left(ang)
+        recur(length / 3, depth - 1, ang)
+        left(ang * (-2))
+        recur(length / 3, depth -1, ang)
+        left(ang)
+        recur(length / 3, depth - 1, ang)
 
+def recur2 (length, depth=0, ang=90):
+    if depth <= 0 :
+        forward(length)
+    else :
+        recur2(length / 3, depth - 1, ang)
+        left(ang)
+        recur2(length / 3, depth - 1, ang)
+        left(-ang)
+        recur2(length / 3, depth -1, ang)
+        left(-ang)
+        recur2(length / 3, depth - 1, ang)
+        left(ang)
+        recur2(length / 3, depth - 1, ang)
 
 # メイン
 i = input("長さの初期値を入力：")
 length = int(i)
+i = input("曲げる角度を入力（30以上90未満）")
+angle = int(i)
 i = input("深さの値を入力：")
 d = int(i)
 
+screensize(1024, 768)
 penup()  # ペンを上げる
-setposition(-400, -200)  # 描き始めの位置を設定
+setposition(-500, -200)  # 描き始めの位置を設定
 pendown()  # ペンを下げる
-recur (length, d)
+if angle <= 60:
+    recur(length, d, angle)
+else:
+    recur2(length, d, angle)
 
 done()  # これがないと描画画面が閉じる
 
