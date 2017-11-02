85b90b20f44ac259fdbff96c34cf157a819bde01
kazzy <vinetengoo3014@gmail.com>
2017-11-02 16:30:45 +0900

    コッホ曲線ベース 全体の長さと深さの調子が可能

---
+++ draw/draw_line.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from turtle import *
+
+def recur (length, depth=0):
+    if depth <= 0 :
+        forward(length)
+    else :
+        recur(length / 3, depth - 1)
+        left(60)
+        recur(length / 3, depth - 1)
+        left(-120)
+        recur(length / 3, depth -1)
+        left(60)
+        recur(length / 3, depth - 1)
+
+
+# メイン
+i = input("長さの初期値を入力：")
+length = int(i)
+i = input("深さの値を入力：")
+d = int(i)
+
+penup()
+setposition(-400, -200)
+pendown()
+recur (length, d)
+
+done()
+
+
+
